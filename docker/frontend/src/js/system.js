0e2319ecd2ce1c7c45598acb8a857b049a1e8499
zodac <arouge110@msn.com>
2021-09-26 15:12:22 +1300

    ACTUALLY fixing the hardware name in UI

--- docker/frontend/src/js/system.js
+++ docker/frontend/src/js/system.js
@@ -201,7 +201,7 @@ function loadHardware() {
                 hardwareOption.setAttribute("display_name", hardwareItem['displayName']);
                 hardwareOption.setAttribute("multiplier", hardwareItem['multiplier']);
 
-                hardwareOption.innerHTML = hardwareItem["hardwareName (" + hardwareItem['displayName'] + ")"];
+                hardwareOption.innerHTML = hardwareItem['hardwareName'] + " (" + hardwareItem['displayName'] + ")";
                 hardwareList.append(hardwareOption);
             });
         }
