4dc002a2aa71b794dd205e57b8f2bd74105266d0
zodac <arouge110@msn.com>
2021-10-24 16:18:41 +1300

    Changing UI to point to internal-secure site for HTTPS

---
+++ folding-stats-war/src/test/java/me/zodac/folding/rest/validator/UserValidatorTest.java
@@ -0,0 +1,14 @@
+package me.zodac.folding.rest.validator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for {@link UserValidator}.
+ */
+class UserValidatorTest {
+
+    @Test
+    public void whenValidatingCreate_givenValidUser_thenSuccessResponseIsReturned() {
+
+    }
+}
