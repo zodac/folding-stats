20189abbb7ddedbb496105a9757e3bfc38cc01eb
zodac <arouge110@msn.com>
2021-11-05 13:13:37 +1300

    Fixing issue with HTML escaping not working on non-strings
    Added a light and dark mode CSS entry (both currently the same style)

--- docker/apache/js/util/html_escaper.js
+++ docker/apache/js/util/html_escaper.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/6234804
 function escape(unsafe) {
-    return unsafe
+    return unsafe.toString()
          .replaceAll('&', "&amp;")
          .replaceAll('<', "&lt;")
          .replaceAll('>', "&gt;")
