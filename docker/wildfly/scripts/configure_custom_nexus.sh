7f36d39038936573dfcad34bb5dea652161eea0d
zodac <arouge110@msn.com>
2021-11-19 09:38:46 +1300

    Allowing use of a custom Nexus repository during backend build phase
    Since you can't mount a volume during the build phase, and that's when we build the artifacts,
    we need to make a settings.xml file on the system before starting the mvn install

---
+++ docker/wildfly/scripts/configure_custom_nexus.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+nexusUrl="${1}"
+
+if [[ ! -z "${nexusUrl}" ]]; then
+  mkdir --parents /root/.m2
+
+  cat << EOF > /root/.m2/settings.xml
+<?xml version="1.0" encoding="UTF-8"?>
+  <settings xmlns="http://maven.apache.org/SETTINGS/1.1.0"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd">
+    <mirrors>
+      <mirror>
+        <id>central</id>
+        <name>central</name>
+        <url>${nexusUrl}/repository/maven-public/</url>
+        <mirrorOf>*</mirrorOf>
+      </mirror>
+    </mirrors>
+  </settings>
+EOF
+else
+  echo "Using default settings.xml"
+fi
