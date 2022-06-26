c7b6ea6911b68ce9d72e862674abc0c127854480
Sheikh Mehdi <sheikh.mehdi@westpac.co.nz>
2022-06-26 19:57:51 +1200

    Initial changes to work

--- docker/backend/scripts/configure_custom_nexus.sh
+++ docker/backend/scripts/configure_custom_nexus.sh
@@ -13,7 +13,7 @@ if [[ -n "${nexusUrl}" ]]; then
       <mirror>
         <id>central</id>
         <name>central</name>
-        <url>${nexusUrl}/repository/maven-public/</url>
+        <url>${nexusUrl}</url>
         <mirrorOf>*</mirrorOf>
       </mirror>
     </mirrors>
