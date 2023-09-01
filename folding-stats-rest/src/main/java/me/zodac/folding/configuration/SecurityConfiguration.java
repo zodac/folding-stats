04c9ef9e0ce920cf2e5a08e062e4357ac6214bea
zodac <arouge110@msn.com>
2023-09-02 01:09:17 +1200

    Disabling CSRF

---
+++ folding-stats-rest/src/main/java/me/zodac/folding/configuration/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+/*
+ * BSD Zero Clause License
+ *
+ * Copyright (c) 2021-2023 zodac.me
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package me.zodac.folding.configuration;
+
+import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * {@link Configuration} class used configure Spring security related options.
+ */
+@Configuration
+@ConditionalOnClass(Gson.class)
+public class SecurityConfiguration {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * Disabled CSRF for the application.
+     *
+     * @return the configured {@link SecurityFilterChain}
+     */
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) {
+        try {
+            return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+        } catch (final Exception e) {
+            LOGGER.error("Error disabling CSRF", e);
+            throw new IllegalStateException("Unable to disable CSRF", e);
+        }
+    }
+}
